af4c414b75c2f65f2e55a08879f9b13aaa61afc9
student <1emonayu2521@gmail.com>
2024-11-18 16:57:13 +0800

    20241118

---
+++ app/src/main/java/yixin/com/example/game2d/Game.kt
@@ -0,0 +1,30 @@
+package yixin.com.example.game2d
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class Game (val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+    var counter = 0
+    val state = MutableStateFlow(0)
+    val background = Background(screenW)
+    var isPlaying = true
+
+    fun Play(){
+        scope.launch {
+            counter = 0
+            while (counter<2000) {
+                delay(40)
+                background.Roll()
+                counter++
+                state.emit(counter)
+
+            }
+        }
+
+
+    }
+
+
+}
