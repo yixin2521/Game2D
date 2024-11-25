bb9d3e5778c6c87fa29ecbe449b3d587cba72395
yixin2521 <1emonayu2521@gmail.com>
2024-11-25 16:16:59 +0800

    2024/11/25

--- app/src/main/java/yixin/com/example/game2d/Game.kt
+++ app/src/main/java/yixin/com/example/game2d/Game.kt
@@ -1,30 +1,63 @@
 package yixin.com.example.game2d
 
+import android.icu.number.Scale
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import android.content.Context
+import android.media.MediaPlayer
 
-class Game (val scope: CoroutineScope, val screenW:Int, val screenH: Int) {
+
+class Game (val scope: CoroutineScope, val screenW:Int, val screenH: Int, scale:Float, context: Context) {
+    //背景音樂
+    var mper1 = MediaPlayer.create(context, R.raw.lastletter)
+    //結束音樂
+    var mper2 = MediaPlayer.create(context, R.raw.gameover)
+
+
     var counter = 0
     val state = MutableStateFlow(0)
     val background = Background(screenW)
+    val boy = Boy(screenH, scale)
+    val virus = Virus(screenW, screenH, scale)
+
     var isPlaying = true
 
     fun Play(){
         scope.launch {
-            counter = 0
-            while (counter<2000) {
+            //counter = 0
+            isPlaying = true
+            while (isPlaying) {
+                mper1.start()  //播放背景音樂
                 delay(40)
                 background.Roll()
+
+                if (counter % 3 == 0) {
+                    boy.Walk()
+                    virus.Fly()
+                    //判斷是否碰撞，結束遊戲
+                    if(boy.getRect().intersect(virus.getRect())) {
+                        isPlaying = false
+                        //遊戲結束音效
+                        mper1.pause()
+                        mper2.start()
+
+                    }
+
+                }
+
                 counter++
                 state.emit(counter)
 
-            }
+              }
+           }
         }
 
-
+    fun Restart(){
+        virus.Reset()
+        counter = 0
+        isPlaying = true
+        Play()
     }
-
-
-}
+     }
--- app/src/main/java/yixin/com/example/game2d/MainActivity.kt
+++ app/src/main/java/yixin/com/example/game2d/MainActivity.kt
@@ -30,6 +30,16 @@ import androidx.compose.ui.unit.IntOffset
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.platform.LocalContext
+import android.app.Activity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
 
 
 
@@ -44,9 +54,9 @@ class MainActivity : ComponentActivity() {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
                     val screenW = resources.displayMetrics.widthPixels
                     val screenH = resources.displayMetrics.heightPixels
-
-                    val game = Game(GlobalScope, screenW,screenH)
-                    Start(m = Modifier.padding(innerPadding), game, screenW)
+                    val scale = resources.displayMetrics.density
+                    val game = Game(GlobalScope, screenW, screenH, scale, this)
+                    Start(m = Modifier.padding(innerPadding), game)
 
 
                 }
@@ -56,7 +66,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Start(m: Modifier, game:Game, screenW:Int) {
+fun Start(m: Modifier, game:Game) {
     val counter by game.state.collectAsState()
     var counter2 by remember { mutableStateOf(0) }
     var msg by remember { mutableStateOf("遊戲開始") }
@@ -81,17 +91,62 @@ fun Start(m: Modifier, game:Game, screenW:Int) {
             .offset { IntOffset(game.background.x2, 0) }
     )
 
+    //繪製小男孩
+    val boyImage = arrayListOf(R.drawable.boy1, R.drawable.boy2,
+        R.drawable.boy3, R.drawable.boy4, R.drawable.boy5,
+        R.drawable.boy6, R.drawable.boy7, R.drawable.boy8)
+
+
+    Image(
+        painter = painterResource(id = boyImage[game.boy.pictNo]),
+        contentDescription = "小男孩",
+        modifier = Modifier
+            .width(100.dp)
+            .height(220.dp)
+            .offset { IntOffset(game.boy.x,game.boy.y) }
+    )
+
+
+    //繪製病毒
+    val virusImage = arrayListOf(R.drawable.virus1, R.drawable.virus2)
+    Image(
+        painter = painterResource(id = virusImage[game.virus.pictNo]),
+        contentDescription = "病毒",
+        modifier = Modifier
+            .size(80.dp)
+            .offset { IntOffset(game.virus.x, game.virus.y) }
+            .pointerInput(Unit) {  //觸控病毒往上，扣一秒鐘
+                detectTapGestures(
+                    onTap = {
+                        game.virus.y -= 40
+                        game.counter -= 25
+                    }
+                )
+            }
+    )
+
+
+    if (msg == "遊戲暫停" && !game.isPlaying) {
+        msg = "遊戲結束，按此按鍵重新開始遊戲"
+    }
+
+
 
     Row {
         Button(
             onClick = {
-                if (msg == "遊戲開始") {
+                if (msg == "遊戲開始"|| msg =="遊戲繼續") {
                     msg = "遊戲暫停"
                     game.Play()
-                } else {
-                    msg = "遊戲開始"
+                } else if (msg=="遊戲暫停"){
+                    msg = "遊戲繼續"
                     game.isPlaying = false
                 }
+                else{  //重新開始遊戲
+                    msg = "遊戲暫停"
+                    game.Restart()
+                }
+
 
             },
             modifier = m
@@ -114,6 +169,21 @@ fun Start(m: Modifier, game:Game, screenW:Int) {
             Text(text = counter2.toString(), modifier = m)
         }
 
+    val activity = (LocalContext.current as? Activity)
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd
+    ){
+        Button(
+            onClick = {
+                game.mper1.stop()
+                game.mper2.stop()
+                activity?.finish()
+            }
+        ) {
+            Text("結束App")
+        }
+    }
 
     }
 
